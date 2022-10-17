8dd8828112b69b6859d16611e2b4008b1129086e
user <user@DESKTOP-COJ9858>
2022-10-17 11:22:00 +0900

    first

---
+++ JavaFirstProject/src/MainClass.java
@@ -0,0 +1,10 @@
+// 실행 => Ctrl+F11
+public class MainClass {
+    // 한개 프로그램의 시작점 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Java!!");
+       
+	}
+
+}
