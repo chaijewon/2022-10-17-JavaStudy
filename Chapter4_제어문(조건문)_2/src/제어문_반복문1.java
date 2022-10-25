9a2a2807b322bf1dec1d5912a5c68b1e6468e71a
user <user@DESKTOP-COJ9858>
2022-10-25 15:53:41 +0900

    1111

--- Chapter4_제어문(조건문)_2/src/제어문_반복문1.java
+++ Chapter4_제어문(조건문)_2/src/제어문_반복문1.java
@@ -31,12 +31,52 @@
  *          for => 일반 for 
  *                 forEach(웹에서 사용) => map , foreach , for of , for in
  *                 -----------------배열 / 컬렉션 
+ *                 
+ *          for 
+ *           1) 형식
+ *                        false => for문 종료 
+ *                   1  → 2 ←  4
+ *              for(초기값;조건식;증감식)
+ *                        ↓    ↗
+ *                   반복수행문장 3
+ *                   
+ *              1부터 10번 수행 
+ *              
+ *              for(int i=1;i<=10;i++)
+ *              {
+ *                  반복문장 
+ *              }
+ *              
+ *              100번 수행 
+ *              for(int i=0;i<100;i++)
+ *              for(int i=1;i<=100;i++)
+ *              A~Z
+ *              for(char c='A';c<='Z';c++)
+ *                         'a'      'z'
+ *               ** 무한 루프 for(;;)
+ *               
+ *               int[] arr={1,2,3,4,5,6,7}
+ *               for(int a:arr)
+ *           2) 동작 순서 
  */
+// 2~9까지 정수 입력을 받아서 해당 단을 출력하는 프로그램 9
+
 public class 제어문_반복문1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        int dan=(int)(Math.random()*8)+2;//2~9
+        System.out.println(dan+"단");
+        for(int i=1;i<=9;i++) // i++ , i+=2 i+=3...
+        {
+        	System.out.printf("%2d * %2d = %2d\n",dan,i,dan*i);
+        	//System.out.println(dan+"*"+i+"="+dan*i);
+        }
 	}
 
 }
+
+
+
+
+
