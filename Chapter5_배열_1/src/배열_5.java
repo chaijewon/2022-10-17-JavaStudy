c2f2be5d17e0703f4b8825a17d774c641984e2c0
user <user@DESKTOP-COJ9858>
2022-10-31 12:51:55 +0900

    1111

---
+++ Chapter5_배열_1/src/배열_5.java
@@ -0,0 +1,24 @@
+/*
+ *     int[] arr=new int[15];
+ *     
+ *     for(int i=0;i<15;i++)
+ *     {
+ *     }
+ *     (X) ===> 수정하기 어렵다 
+ *     
+ *     ==> 배열의 갯수를 읽어 온다 : length
+ *     ==> arr.length
+ */
+public class 배열_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int[] arr={10,20,30,40,50,60,70,80,90};
+        System.out.println("배열의 갯수:"+arr.length);
+        for(int i=0;i<arr.length;i++)
+        {
+        	System.out.println(arr[i]);
+        }
+	}
+
+}
