a6f6a652447c7f4afd638198444d1a64dd0cfe24
user <user@DESKTOP-COJ9858>
2022-11-25 14:41:29 +0900

    1111

--- Chapter12_제네릭스_1/src/com/sist/music/MusicSystem.java
+++ Chapter12_제네릭스_1/src/com/sist/music/MusicSystem.java
@@ -1,6 +1,10 @@
 package com.sist.music;
 // 지니 , 멜론 ==> 데이터를 저장 => 요청에 대한 처리
 import java.util.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 public class MusicSystem {
    private static ArrayList<Music> list=new ArrayList<Music>();//200개 저장 
    /*
@@ -69,7 +73,77 @@ public class MusicSystem {
 		   // 지니뮤직 
 		   for(int i=1;i<=2;i++)
 		   {
-			   
+			   Document doc=Jsoup.connect("https://www.genie.co.kr/chart/top200?ditc=D&ymd=20221125&hh=14&rtm=Y&pg="+i).get();
+			   Elements title=doc.select("table.list-wrap td.info a.title");
+			   Elements singer=doc.select("table.list-wrap td.info a.artist");
+			   Elements album=doc.select("table.list-wrap td.info a.albumtitle");
+			   Elements etc=doc.select("span.rank span.rank");
+			   for(int j=0;j<title.size();j++)
+			   {
+				   /*System.out.println(title.get(j).text()+" "
+						   +singer.get(j).text()+" "
+						   +album.get(j).text()+" "
+						   +etc.get(j).text());*/
+				   Music m=new Music(); // m=>100개
+				   m.setMno(mno);
+				   m.setCno(1);
+				   m.setTitle(title.get(j).text());
+				   m.setSinger(singer.get(j).text());
+				   m.setAlbum(album.get(j).text());
+				   String temp=etc.get(j).text();
+				   int in=0;
+				   // 2하강
+				   String state=temp.replaceAll("[^가-힣]", "");
+				   if(state.equals("유지"))
+				   {
+					   in=0;
+				   }
+				   else 
+				   {
+					   in=Integer.parseInt(temp.replaceAll("[^0-9]", "").trim());
+				   }
+				   m.setState(state);
+				   m.setCrement(in);
+				   list.add(m);//ArrayList는 Music객체만 저장이 가능 
+				   mno++;
+			   }
+			  
+		   }
+		   // 데이터 수집 완료 
+		   mno=101;
+		   Document doc=Jsoup.connect("https://www.melon.com/chart/index.htm").get();
+		   Elements title=doc.select("tr.lst50 div.rank01");
+		   Elements singer=doc.select("tr.lst50 div.rank02");
+		   Elements album=doc.select("tr.lst50 div.rank03");
+		   Elements etc=doc.select("tr.lst50 div.wrap span.rank_wrap");
+		   for(int i=0;i<title.size();i++)
+		   {
+			   /*System.out.println(title.get(i).text()+" "
+					   +singer.get(i).text()+" "
+					   +album.get(i).text()+" "
+					   +etc.get(i).text());*/
+			   Music m=new Music(); // m=>100개
+			   m.setMno(mno);
+			   m.setCno(1);
+			   m.setTitle(title.get(i).text());
+			   m.setSinger(singer.get(i).text());
+			   m.setAlbum(album.get(i).text());
+			   String temp=etc.get(i).text();
+			   int in=0;
+			   // 2하강
+			   String state=temp.replaceAll("[^가-힣]", "");
+			   if(state.contains("동일"))
+			   {
+				   in=0;
+			   }
+			   else 
+			   {
+				   in=Integer.parseInt(temp.replaceAll("[^0-9]", "").trim());
+			   }
+			   m.setState(state);
+			   m.setCrement(in);
+			   list.add(m);//ArrayList는 Music객체만 저장이 가능 
+			   mno++;
 		   }
 	   }catch(Exception ex){}
    }
@@ -78,4 +152,14 @@ public class MusicSystem {
    // 노래 검색 (노래 / 가수명)
    // 상세보기 ==> mno
    // 댓글 / 추천 .... 음반 구매 , 포인트 충전 ....
+   public static void main(String[] args) {
+	  MusicSystem m=new MusicSystem();
+	  System.out.println(m.list.size());
+   }
 }
+
+
+
+
+
+
